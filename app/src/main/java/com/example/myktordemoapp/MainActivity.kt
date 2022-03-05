1e984d87d5041bef6196f3a86f0a44d9973f787a
GautamDeepika <97622775+GautamDeepika@users.noreply.github.com>
2022-03-05 11:39:41 +0530

    -Onboarding screen implementation: done

--- app/src/main/java/com/example/myktordemoapp/MainActivity.kt
+++ app/src/main/java/com/example/myktordemoapp/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.myktordemoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.myktordemoapp.navigation.SetUpNavGraph
 import com.example.myktordemoapp.ui.theme.MyKtorDemoAppTheme
+import com.google.accompanist.pager.ExperimentalPagerApi
 import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
+@ExperimentalAnimationApi
+@ExperimentalPagerApi
 class MainActivity : ComponentActivity() {
 
     private lateinit var navController: NavHostController
